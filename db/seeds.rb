9d7b75824d6f9e3898ba2c2ed5ef295cce155e4a
Alec Magnet <amagnet@gmail.com>
2021-08-19 10:19:47 -0400

    complete deliverables

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,14 @@ Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
 
-# ***************************************************************
-# * TODO: create freebies! Remember, a freebie belongs to a dev *
-# * and a freebie belongs to a company.                         *
-# ***************************************************************
-# Create freebies Here
+Freebie.create(item_name: "mouse pad", value: 1, company_id: 1, dev_id: 1)
+Freebie.create(item_name: "mouse pad", value: 1, company_id: 1, dev_id: 2)
+Freebie.create(item_name: "mouse pad", value: 1, company_id: 1, dev_id: 3)
+Freebie.create(item_name: "mug", value: 2, company_id: 2, dev_id: 1)
+Freebie.create(item_name: "mug", value: 2, company_id: 2, dev_id: 2)
+Freebie.create(item_name: "mug", value: 2, company_id: 2, dev_id: 3)
+Freebie.create(item_name: "tote bag", value: 1, company_id: 2, dev_id: 1)
+Freebie.create(item_name: "tote bag", value: 1, company_id: 2, dev_id: 2)
+Freebie.create(item_name: "tote bag", value: 1, company_id: 2, dev_id: 3)
 
 puts "Seeding done!"
